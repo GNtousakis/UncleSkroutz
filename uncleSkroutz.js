664624675dffe41254a1c9f19345e43a1ada30b4
GNtousakis <greco1996@gmail.com>
2020-02-01 20:10:25 +0200

    Updated

--- uncleSkroutz.js
+++ uncleSkroutz.js
@@ -1,13 +1,36 @@
-const argv = require('yargs').argv;
+#!/usr/bin/env node
+const argv = require('yargs')
+	.options({
+    'add': {
+      alias: 'a',
+      describe: 'add product',
+    },
+    'show': {
+      alias: 's',
+      describe: 'show prices from list',
+    },
+    'run': {
+      alias: 'r',
+      describe: 'run price tracker for latest prices'
+    }
+  	})
+	.argv;
+
 const url = require('url');
 const chalk = require('chalk');
 const utils = require('./lib/utils.js')();
 
-if (argv.page === undefined) {
-   	console.log(chalk.bold.red("You didnt gave a link"));
-} else {
-    const site = utils.getHost(argv.page);
+if (argv.add != undefined && argv.add != true) {
+    const site = utils.getHost(argv.add);
     const crawler = require("./lib/crawlers/" + site + ".js")();
-   	crawler.scrapPage(argv.page);
+   	crawler.scrapPage(argv.add);
 };
+
+if (argv.show != undefined) {
+	utils.showPrices();
+}
+
+if (argv.run != undefined) {
+	utils.getLatestPrices();
+}
 
